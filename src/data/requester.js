2f403db826f542b6efc8b9d77c9fc9c3c0625d3e
ktodorov12 <k_todorov12@abv.bg>
2024-04-06 22:40:49 +0300

    Add function to make requests to server

---
+++ src/data/requester.js
@@ -0,0 +1,74 @@
+import { clearUserData, getUserToken } from "../util.js";
+
+const host = "https://parseapi.back4app.com";
+const appId = "wiGlkOqBJqWcycdj7p7yhQbgwk6qksF26LhMIydm";
+const apiKey = "BDfgK1OMAfyEIuJkFNRz0bfFmrDru2rbIAoaY25C";
+
+/**
+ *
+ * @param {string} method
+ * @param {string} url
+ * @param {Object} data
+ */
+async function requester(method, url, data) {
+  /**
+   * @type {{method: string, headers: {}, body: object}}
+   */
+  const options = {
+    method,
+    headers: {
+      "X-Parse-Application-Id": appId,
+      "X-Parse-JavaScript-Key": apiKey,
+    },
+    body: {},
+  };
+
+  if (data) {
+    options.headers["Content-Type"] = "application/json";
+    options.body = JSON.stringify(data);
+  }
+
+  const token = getUserToken();
+  if (token) {
+    options.headers["X-Parse-Session-Token"] = token;
+  }
+
+  try {
+    const response = await fetch(host + url, options);
+
+    if (!response.ok) {
+      const error = await response.json();
+
+      if (error.code == 209) {
+        clearUserData();
+      }
+      throw new Error(error.message);
+    }
+
+    if (response.status == 204) {
+      return response;
+    } else {
+      return response.json();
+    }
+  } catch (error) {
+    //TODO if error is required
+    alert(error);
+    throw error;
+  }
+}
+
+/** @type {(url: string) => Promise<any>}*/
+export const get = (url) => requester("get", url);
+/** @type {(url: string, data: object) => Promise<any>}*/
+export const post = (url, data) => requester("post", url, data);
+/** @type {(url: string, data: object) => Promise<any>}*/
+export const put = (url, data) => requester("put", url, data);
+/** @type {(url: string) => Promise<any>}*/
+export const del = (url) => requester("delete", url);
+
+/**
+ * @typedef {Object} Pointer
+ * @property {string} _type
+ * @property {string} className
+ * @property {string} objectId
+ */
