61039e40541dd62333972ae8082ef80f3d703eca
ktodorov12 <k_todorov12@abv.bg>
2024-04-07 14:50:03 +0300

    Create new navigation template

--- src/data/users.js
+++ src/data/users.js
@@ -1,5 +1,5 @@
-import { clearUserData, getUserData, setUserData } from "../util.js";
-import { post } from "./requester.js";
+import { clearUserData, setUserData } from "@src/util.js";
+import { post } from "@src/data/requester.js";
 
 const endpoints = {
   login: "/login",
@@ -11,26 +11,16 @@ const endpoints = {
 export async function login(data) {
   const loginData = await post(endpoints.login, data);
   setUserData(loginData);
-  updateNav();
 }
 
 /** @param {{username: string, password: string, email:string}} data */
 export async function register(data) {
   const registerData = await post(endpoints.register, data);
   setUserData(registerData);
-  updateNav();
 }
 
 export async function logout() {
   const promise = post(endpoints.logout, {});
   clearUserData();
-  updateNav();
   await promise;
-}
-
-export function updateNav() {
-  //TODO update the corresponding nav
-  const userdata = getUserData();
-  document.querySelector(".user").style.display = userdata ? "inline-block" : "none";
-  document.querySelector(".guest").style.display = userdata ? "none" : "inline-block";
 }
---
+++ src/views/navigation.js
@@ -0,0 +1,34 @@
+import { html } from "@lit-html/lit-html.js";
+import { logout } from "@src/data/users.js";
+
+/**
+ * @param {import("@src/types").PageContext} ctx  
+ * @returns {import('@lit-html/lit-html').TemplateResult} Returns an HTML template result.
+*/
+export function navigationTemplate(ctx) {
+  return html` 
+  <header id="titlebar">
+    <nav>
+      <a class="logotype" href="/"><i class="fas fa-question-circle"></i><i class="merge fas fa-check-circle"></i><span>Quiz Fever</span></a>
+      <div class="navigation">
+        <a class="nav-link" href="/browse">Browse</a>
+        ${!!ctx.user 
+            ? html`
+            <div id="user-nav">
+                <a class="nav-link" href="/create">Create</a>
+                <a class="nav-link profile-link" href="#"><i class="fas fa-user-circle"></i></a>
+                <a id="logoutBtn" class="nav-link" @click=${() => onLogout(ctx.page)} href="javascript:void(0)">Logout</a>
+            </div>`
+            : html`           
+            <div id="guest-nav">
+                <a class="nav-link" href="/login">Sign in</a>
+            </div>`}
+      </div>
+    </nav>
+  </header>`;
+}
+
+async function onLogout(page) {
+  await logout();
+  page.redirect("/")
+}
