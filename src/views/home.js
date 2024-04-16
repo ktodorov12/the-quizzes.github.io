1a333b332d53012d0af8d0a530cc3c23e946b0e7
ktodorov12 <k_todorov12@abv.bg>
2024-04-16 19:16:33 +0300

    Change logic in home view to render first 3 quizzes, change links, make the template dinamic

--- src/views/home.js
+++ src/views/home.js
@@ -1,50 +1,41 @@
 import { html } from "@lit-html/lit-html.js";
+import { getAllData } from "@src/data/data.js";
+import { updateTopic } from "@src/data/dataUpdate.js";
 import { navigationTemplate } from "@src/views/navigation.js";
+import { quizCard } from "@src/views/partials.js";
 
 /**
- * 
- * @param {import("@src/types").PageContext} ctx 
+ *
+ * @param {import("@src/types").PageContext} ctx
  * @returns {import('@lit-html/lit-html.js').TemplateResult} Returns an HTML template result.
  */
-function homeTemplate(ctx) {
-  return html` 
-  ${navigationTemplate(ctx)}
-  <section id="welcome">
-    <div class="hero layout">
-      <div class="splash right-col"><i class="fas fa-clipboard-list"></i></div>
-      <div class="glass welcome">
-        <h1>Welcome to Quiz Fever!</h1>
-        <p>Home to 157 quizes in 12 topics. <a href="#">Browse all quizes</a>.</p>
-        <a class="action cta" href="#">Sign in to create a quiz</a>
-      </div>
-    </div>
-
-    <div class="pad-large alt-page">
-      <h2>Our most recent quiz:</h2>
-
-      <article class="preview layout">
-        <div class="right-col">
-          <a class="action cta" href="#">View Quiz</a>
+function homeTemplate(ctx, quizes) {
+  return html` ${navigationTemplate(ctx)}
+    <section id="welcome">
+      <div class="hero layout">
+        <div class="splash right-col"><i class="fas fa-clipboard-list"></i></div>
+        <div class="glass welcome">
+          <h1>Welcome to Quiz Fever!</h1>
+          <p>Home to ${quizes.length} quizes in 4 topics. <a href="/dashboard">Browse all quizes</a>.</p>
+          ${ctx.user 
+            ? html`<a class="action cta" href="/create">Create quiz</a>` 
+            : html`<a class="action cta" href="/login">Sign in to create a quiz</a>`}
         </div>
-        <div class="left-col">
-          <h3>Extensible Markup Language</h3>
-          <span class="quiz-topic">Topic: Languages</span>
-          <div class="quiz-meta">
-            <span>15 questions</span>
-            <span>|</span>
-            <span>Taken 54 times</span>
-          </div>
-        </div>
-      </article>
-
-      <div>
-        <a class="action cta" href="#">Browse all quizes</a>
       </div>
-    </div>
-  </section>`;
+
+      <div class="pad-large alt-page">
+        <h2>Our most recent quiz:</h2>
+          ${quizes.slice(0, 3).map(quizCard)}
+        <div>
+          <a class="action cta" href="/dashboard">Browse all quizes</a>
+        </div>
+      </div>
+    </section>`;
 }
 
 /**@param {import("@src/types").PageContext} ctx */
-export function showHome(ctx) {
-    ctx.render(homeTemplate(ctx));
+export async function showHome(ctx) {
+  const quizzes = await getAllData("quizzes");
+  const updated = await updateTopic(quizzes);
+  ctx.render(homeTemplate(ctx, updated));
 }
