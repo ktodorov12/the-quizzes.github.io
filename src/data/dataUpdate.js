9ffe4ee85daf872ef0bbad3a42a4cb437b127bcb
ktodorov12 <k_todorov12@abv.bg>
2024-04-16 19:19:35 +0300

    Create stand alone module for manipulating data and create function to manipulate the topic of quiz

---
+++ src/data/dataUpdate.js
@@ -0,0 +1,20 @@
+import { getAllData } from "./data";
+
+/**
+ * Takes a quiz or quizzes and changes its topic in the corresponding one
+ * @param {Object} quiz 
+ * @returns 
+ */
+export async function updateTopic(quiz) {
+    const topics = await getAllData("quizTopic");
+  
+    const updated = await Promise.all(
+      quiz.results.map((quiz) => {
+        const correctTopic = topics.results.find((q) => q.value == quiz.topic);
+        quiz.topic = correctTopic.textContent;
+        return quiz;
+      })
+    );
+  
+    return updated;
+  }
