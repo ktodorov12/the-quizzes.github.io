ed4fe4985d2b39618ccca09c6523fa0108249bc6
ktodorov12 <k_todorov12@abv.bg>
2024-04-17 21:11:47 +0300

    add functionalitty for getting user by given id and all items of quiz by given quizID

--- src/data/data.js
+++ src/data/data.js
@@ -4,6 +4,8 @@ const endpoints = {
   allData: (apiClass) => `/classes/${apiClass}`,
   request: (apiClass, id) => `/classes/${apiClass}/${id}`,
   search: (apiClass, whereClause) => `/classes/${apiClass}?where=${encodeURIComponent(JSON.stringify(whereClause))}`,
+  user: (userId) => `/users/${userId}`,
+  questionsByQuiz: (className, quizId) => `/classes/${className}?where={"quizId": {"__type":"Pointer","className":"quizzes","objectId":"${quizId}"}}`
 };
 
 /**
@@ -52,6 +54,13 @@ export async function deleteData(apiClass, id) {
   return await del(endpoints.request(apiClass, id));
 }
 
+/**
+ * Function to search for quiz by title and topic
+ * @param {string} apiClass - The classes which are putted in the URL for the back4app request, they might be - questions; quizzes; solutions; quizTopic
+ * @param {string} title - if there is no title it empty string should be given, for it to search all quizzes in a topic
+ * @param {string} topic - Topic is either all or specified by the user, there must always be topic 
+ * @returns {Promise<Object>}
+ */
 export async function search(apiClass, title, topic) {
   let whereClause = { title: { $regex: title, $options: "i" } };
   if (topic !== "all") {
@@ -60,3 +69,15 @@ export async function search(apiClass, title, topic) {
   return await get(endpoints.search(apiClass, whereClause));
 }
 
+/**
+ * Gets data for a user by the user ID
+ * @param {string} userId 
+ * @returns {Promise<Object>}
+ */
+export async function getUserById(userId) {
+  return await get(endpoints.user(userId));
+}
+
+export async function getAllItemsForOneQuiz(className, quizId) {
+  return await get(endpoints.questionsByQuiz(className, quizId));
+}
