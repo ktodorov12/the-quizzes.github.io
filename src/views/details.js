6adfc869d7425e6ca8a63b5101509eac0f311eae
ktodorov12 <k_todorov12@abv.bg>
2024-04-20 20:53:46 +0300

    Update details profile href and add check for contest button

--- src/views/details.js
+++ src/views/details.js
@@ -1,22 +1,25 @@
 import { html } from "@lit-html/lit-html.js";
 import { getDataDetails, getUserById } from "@src/data/data.js";
 import { updateTopic } from "@src/data/dataUpdate.js";
+import { isOwner } from "@src/util.js";
 import { navigationTemplate } from "@src/views/navigation.js";
 
 /**
  * 
  * @param {import("@src/types").PageContext} ctx 
  * @param {Object} quiz 
+ * @param {import("@src/types").UserSession} creator 
+ * @param {Boolean} isOwner 
  * @returns {import("@lit-html/lit-html.js").TemplateResult}
  */
-function detailsTemplate(ctx, quiz) {
+function detailsTemplate(ctx, quiz, creator, isOwner) {
   return html` 
   ${navigationTemplate(ctx)}
     <section id="details">
       <div class="pad-large alt-page">
         <article class="details">
           <h1>${quiz.title}</h1>
-          <span class="quiz-topic">A quiz by <a href="#">${quiz.ownerName.username}</a> on the topic of ${quiz.topic}</span>
+          <span class="quiz-topic">A quiz by <a href="/profile/${creator.objectId}">${creator.username}</a> on the topic of ${quiz.topic}</span>
           <div class="quiz-meta">
             <span>${quiz.questionCount} Questions</span>
             <span>|</span>
@@ -24,7 +27,7 @@ function detailsTemplate(ctx, quiz) {
           </div>
           <p class="quiz-desc">${quiz.description}</p>
 
-          ${!!ctx.user
+          ${!!ctx.user && !isOwner
             ? html` <div>
                 <a class="cta action" href="/contest/${quiz.objectId}/${1}">Begin Quiz</a>
               </div>`
@@ -43,6 +46,7 @@ export async function showDetails(ctx) {
   const quizId = ctx.params.id;
   const quizDetails = await getDataDetails("quizzes", quizId);
   const updated = await updateTopic(quizDetails);
-  updated[0].ownerName = await getUserById(updated[0].ownerId.objectId);
-  ctx.render(detailsTemplate(ctx, updated[0]));
+  const creator = await getUserById(updated[0].ownerId.objectId);
+  const owner = isOwner(creator.objectId)
+  ctx.render(detailsTemplate(ctx, updated[0], creator, owner));
 }
